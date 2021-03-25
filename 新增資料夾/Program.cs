c065ba240a02f310adad76043ab2601fc1801527
SHINYEEE <1106105108@nkust.edu.tw>
2021-03-25 12:03:11 +0800

    111

---
+++ 新增資料夾/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            StreamReader r = new StreamReader("公共充電站.json");
+            string json = r.ReadToEnd();
+
+            JsonTextReader reader = new JsonTextReader(new StringReader(json));
+            while (reader.Read())
+            {
+                if (reader.Value != null)
+                {
+                    if (reader.TokenType == JsonToken.PropertyName)
+                        Console.WriteLine("PropertyName: {0}", reader.Value);
+                    else if (reader.TokenType == JsonToken.String)
+                        Console.WriteLine("Value: {0}", reader.Value);
+                }
+                else
+                {
+                    Console.WriteLine("Token: {0}", reader.TokenType);
+                }
+            }
+            Console.Read();
+        }
+    }
+
+}
